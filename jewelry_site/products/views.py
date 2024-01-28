0514f7113058d015f8de3e0b00fcba5a7aada7d0
solajaa <solajaded.gmail.com>
2024-01-28 12:55:19 -0500

    site update

--- jewelry_site/products/views.py
+++ jewelry_site/products/views.py
@@ -1,3 +1,10 @@
+# products/views.py
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+def your_view_name(request):
+    print("View is being called")
+    # Rest of your view logic
